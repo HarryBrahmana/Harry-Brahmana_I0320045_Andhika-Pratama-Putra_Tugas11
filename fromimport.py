492978551abd2955d28dc43458f3820891d40c1f
HarryBrahmana <80480107+HarryBrahmana@users.noreply.github.com>
2021-05-21 19:36:06 +0700

    Add files via upload
    Harry-Brahmana_I0320045_Andhika-Pratama-Putra_Tugas11

---
+++ fromimport.py
@@ -0,0 +1,15 @@
+#nama file: fromimport.py
+#mengimpor fungsi luasPersegiPanjang
+#dari modul geometri2D
+from matematika.geometri2D import luasPersegiPanjang
+
+#persegi panjang
+p = 10
+l = 8
+
+luas = luasPersegiPanjang(p, l)
+
+print("Persegi Panjang")
+print("Panjang\t:", p)
+print("Lebar\t:", l)
+print("luas\t:", luas)
